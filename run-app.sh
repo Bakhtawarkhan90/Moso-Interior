595a71d15278d96cd924c8f8e817766e08d462d7
BAKHTAWAR KHAN <bakhtawarkhan1367@gmail.com>
2025-02-10 23:00:49 +0530

    Create run-app.sh

---
+++ run-app.sh
@@ -0,0 +1,22 @@
+# Downloading Docker and Docker Compose
+
+echo "****************************** Installing Docker and Docker Compose **************************"
+sudo apt update && sudo apt-get install -y docker.io
+sudo apt-get install docker-compose-v2
+# Adding the currently logged-in user and Jenkins user to the Docker group
+echo "****************************** Adding users to Docker group **************************"
+sudo chown $USER /var/run/docker.sock
+sudo usermod -aG docker $USER && sudo apt update  -y
+
+echo "**************************DOCKER VERSION***************************"
+sudo docker --version
+echo "*********************DOCKER-COMPOSE VERSION*************************"
+sudo docker compose version
+
+
+echo "**************************RUNNING PROJECT*************************** "
+cd /home/ubuntu/Moso-Interior
+docker compose down
+docker compose up --build -d
+
+echo "**************************NOW YOU CAN ACCESS APP ON PORT 5000 *************************** "
