4a9848704522ceb6b32f8a18390f558407af8e86
Bakhtawarkham90 <bakhtawarkhan1367@gmail.com>
2025-04-05 10:20:26 +0530

    moso

--- run-app.sh
+++ run-app.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
+
 # Downloading Docker and Docker Compose
-echo "*************Clonning code************ "
-git clone https://github.com/Bakhtawarkhan90/Moso-Interior.git
-cd Moso-Interior
 echo "****************************** Installing Docker and Docker Compose **************************"
 sudo apt update && sudo apt-get install -y docker.io
 sudo apt-get install docker-compose-v2
+
 # Adding the currently logged-in user and Jenkins user to the Docker group
 echo "****************************** Adding users to Docker group **************************"
 sudo chown $USER /var/run/docker.sock
 sudo usermod -aG docker $USER && sudo apt update  -y
 
+# Verify Installation
 echo "**************************DOCKER VERSION***************************"
 sudo docker --version
 echo "*********************DOCKER-COMPOSE VERSION*************************"
 sudo docker compose version
 
-
+# Running project in one goo
 echo "**************************RUNNING PROJECT*************************** "
 git clone https://github.com/Bakhtawarkhan90/Moso-Interior.git
 cd /home/ubuntu/Moso-Interior
 docker compose down
 docker compose up --build -d
 
-echo "**************************NOW YOU CAN ACCESS APP ON PORT 5000 *************************** "
+echo "##############################################################################################################################################"
+
+echo "************************** NOW YOU CAN ACCESS APP ON PORT 5000 *************************** "
+
+echo "##############################################################################################################################################"
